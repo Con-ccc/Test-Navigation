e95ef8d896409eb457d7e2dacaa83adf23ee606a
ConC <Con.CCC@Gmail.com>
2024-11-17 08:55:14 +1100

    Initial Commit

---
+++ Test Navigation/Test_NavigationApp.swift
@@ -0,0 +1,17 @@
+//
+//  Test_NavigationApp.swift
+//  Test Navigation
+//
+//  Created by Con Coucoumakis on 17/11/2024.
+//
+
+import SwiftUI
+
+@main
+struct Test_NavigationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
